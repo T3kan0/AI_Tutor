341127bd5e6a8810b8dff3575981dd4815b44916
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-09-30 10:32:40 +0200

    Update tutor.py

--- tutor.py
+++ tutor.py
@@ -10,7 +10,11 @@ import textwrap
 # Load API key from Streamlit secrets
 api_key = st.secrets["groq"]["api_key"]
 # OpenAI 8000 tokens
-client = Groq(api_key=api_key)
+#client = Groq(api_key=api_key)
+
+client = OpenAI(api_key=api_key])
+
+
 
 
 template = """
