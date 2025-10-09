53e02caa11a48433d0f6a114a4db02f116309b0e
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2025-10-09 12:07:09 +0200

    Update tutor.py

--- tutor.py
+++ tutor.py
@@ -245,7 +245,7 @@ else:
         
         genre = st.sidebar.radio(
             "What's your favorite movie genre",
-            [":rainbow[Tutor Session Mode]", "***Material Engagement***"]
+            [":rainbow[Tutor Session Mode]", "***Material Engagement***"],
             captions=[
                 "Engage with the GenAI tutor through CHAT",
                 "Material Assistance with GenAI",
